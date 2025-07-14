7b568ac92dc95938221a0965142e96b8cd6bf446
Ashton <ashton.smith2123@gmail.com>
2025-07-13 22:33:17 -0400

    input system p1

--- Engine/src/core/application.c
+++ Engine/src/core/application.c
@@ -1,6 +1,7 @@
 #include "application.h"
 #include "game_types.h"
 #include "core/event.h"
+#include "core/input.h"
 
 #include "logger.h"
 
@@ -30,6 +31,7 @@ b8 application_create(game* game_inst) {
 
     // Initialize subsystems.
     initialize_logging();
+    input_initialize();
 
     // TODO: Remove this
     AVFATAL("A test message: %f", 3.14f);
@@ -91,12 +93,19 @@ b8 application_run() {
                 app_state.is_running = FALSE;
                 break;
             }
+            
+            // NOTE: Input update/state copying should always be handled
+            // after any input should be recorded; I.E. before this line.
+            // As a safety, input is the last thing to be updated before
+            // this frame ends.
+            input_update(0);
         }
     }
 
     app_state.is_running = FALSE;
 
     event_shutdown();
+    input_shutdown();
 
     platform_shutdown(&app_state.platform);
 
