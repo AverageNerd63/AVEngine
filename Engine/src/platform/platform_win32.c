285e96d8d69abd2418a3f3dfdb77610ec6d8e46a
Ashton <ashton.smith2123@gmail.com>
2025-07-14 17:55:34 -0400

    Vulkan Setup; Physical Device, Surface

--- Engine/src/platform/platform_win32.c
+++ Engine/src/platform/platform_win32.c
@@ -10,9 +10,15 @@
 #include <windowsx.h>
 #include <stdlib.h>
 
+// For Surface Creation
+#include <vulkan/vulkan.h>
+#include "vulkan/vulkan_win32.h"
+#include "renderer/vulkan/vulkan_types.inl"
+
 typedef struct internal_state {
     HINSTANCE h_instance;
     HWND hwnd;
+    VkSurfaceKHR surface;
 } internal_state;
 
 // Clock
@@ -184,6 +190,26 @@ f64 platform_get_absolute_time() {
 void platform_sleep(u64 ms) {
     Sleep(ms);
 }
+
+// Surface creation for Vulkan
+b8 platform_create_vulkan_surface(platform_state *plat_state, vulkan_context *context) {
+    // Simply cold-cast to the known type.
+    internal_state *state = (internal_state *)plat_state->internal_state;
+
+    VkWin32SurfaceCreateInfoKHR create_info = {VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR};
+    create_info.hinstance = state->h_instance;
+    create_info.hwnd = state->hwnd;
+
+    VkResult result = vkCreateWin32SurfaceKHR(context->instance, &create_info, context->allocator, &state->surface);
+    if (result != VK_SUCCESS) {
+        AVFATAL("Vulkan surface creation failed.");
+        return FALSE;
+    }
+
+    context->surface = state->surface;
+    return TRUE;
+}
+
 
 void platform_get_required_extension_names(const char ***names_darray) {
     darray_push(*names_darray, &"VK_KHR_win32_surface");
