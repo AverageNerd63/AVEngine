2ccec5e30a21b9b1506b106709f5f85f6b87db81
Ashton <ashton.smith2123@gmail.com>
2025-07-14 15:58:35 -0400

    Vulkan Setup: Extension, Validatation, Debugger

--- Engine/src/core/application.c
+++ Engine/src/core/application.c
@@ -2,6 +2,9 @@
 #include "game_types.h"
 #include "core/event.h"
 #include "core/input.h"
+#include "core/clock.h"
+
+#include "renderer/renderer_frontend.h"
 
 #include "logger.h"
 
@@ -15,6 +18,7 @@ typedef struct application_state {
     platform_state platform;
     i16 width;
     i16 height;
+    clock clock;
     f64 last_time;
 } application_state;
 
@@ -67,6 +71,12 @@ b8 application_create(game* game_inst) {
         return FALSE;
     }
 
+    // Renderer startup
+    if (!renderer_initalize(game_inst->app_config.name, &app_state.platform)) {
+        AVFATAL("Game failed to initialize.");
+        return FALSE;
+    }
+
     // Initialize the game.
     if (!app_state.game_inst->initialize(app_state.game_inst)) {
         AVFATAL("Game failed to initialize.");
@@ -81,6 +91,13 @@ b8 application_create(game* game_inst) {
 }
 
 b8 application_run() {
+    clock_start(&app_state.clock);
+    clock_update(&app_state.clock);
+    app_state.last_time = app_state.clock.elapsed;
+    f64 running_time = 0;
+    u8 frame_count = 0;
+    f64 target_frame_seconds = 1.0f / 60;
+
     AVINFO(get_memory_usage_str());
 
     while (app_state.is_running) {
@@ -89,24 +106,55 @@ b8 application_run() {
         }
 
         if (!app_state.is_suspended) {
-            if (!app_state.game_inst->update(app_state.game_inst, (f32)0)) {
+            // Update clock and get delta time
+            clock_update(&app_state.clock);
+            f64 current_time = app_state.clock.elapsed;
+            f64 delta = (current_time - app_state.clock.elapsed);
+            f64 frame_start_time = platform_get_absolute_time();
+
+            if (!app_state.game_inst->update(app_state.game_inst, (f32)delta)) {
                 AVFATAL("Game update failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
             }
 
             // Call the game's render routine.
-            if (!app_state.game_inst->render(app_state.game_inst, (f32)0)) {
+            if (!app_state.game_inst->render(app_state.game_inst, (f32)delta)) {
                 AVFATAL("Game render failed, shutting down.");
                 app_state.is_running = FALSE;
                 break;
+            }
+
+            // TODO: Refactor Packet creation
+            render_packet packet;
+            packet.delta_time = delta;
+            renderer_draw_frame(&packet);
+
+            // Figure out how long the frame took and, if below
+            f64 frame_end_time = platform_get_absolute_time();
+            f64 frame_elapsed_time = frame_end_time - frame_start_time;
+            running_time += frame_elapsed_time;
+            f64 remaining_seconds = target_frame_seconds - frame_elapsed_time;
+
+            if (remaining_seconds > 0) {
+                u64 remaining_ms = (remaining_seconds * 1000);
+
+                // If there is time left, give it back to the OS.
+                b8 limit_frames = FALSE;
+                if (remaining_ms > 0 && limit_frames) {
+                    platform_sleep(remaining_ms - 1);
+                }
+
+                frame_count++;
             }
             
             // NOTE: Input update/state copying should always be handled
             // after any input should be recorded; I.E. before this line.
             // As a safety, input is the last thing to be updated before
             // this frame ends.
-            input_update(0);
+            input_update(delta);
+
+            app_state.last_time = current_time;
         }
     }
 
@@ -118,6 +166,8 @@ b8 application_run() {
     event_unregister(EVENT_CODE_KEY_RELEASED, 0, application_on_key);
     event_shutdown();
     input_shutdown();
+
+    renderer_shutdown();
 
     platform_shutdown(&app_state.platform);
 
--- Engine/src/core/avstring.h
+++ Engine/src/core/avstring.h
@@ -6,3 +6,6 @@
 AVAPI u64 string_length(const char* str);
 
 AVAPI char* string_duplicate(const char* str);
+
+// Case-sensitive string comparison. True if the same, otherwise false.
+AVAPI b8 strings_equal(const char* str0, const char* str1);
