592db6ee0d4e701af52087ef3958fa43be8f8fe5
Ashton <ashton.smith2123@gmail.com>
2025-07-13 22:52:31 -0400

    input linux

---
+++ Engine/src/core/avstring.c
@@ -0,0 +1,15 @@
+#include "core/avstring.h"
+#include "core/avmemory.h"
+
+#include <string.h>
+
+u64 string_length(const char* str) {
+    return strlen(str);
+}
+
+char* string_duplicate(const char* str) {
+    u64 length = string_length(str);
+    char* copy = avallocate(length + 1, MEMORY_TAG_STRING);
+    avcopy_memory(copy, str, length + 1);
+    return copy;
+}
