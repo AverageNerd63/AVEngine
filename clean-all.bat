2183e52e03e2c35d632d3d6057c2255e65dc79b4
Ashton <ashton.smith2123@gmail.com>
2025-07-15 23:28:40 -0400

    Build System Update

---
+++ clean-all.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Clean Everything
+
+ECHO "Cleaning Everything..."
+
+REM Engine
+make -f "Makefile.engine.windows.mak" clean
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+REM Testbed
+make -f "Makefile.testbed.windows.mak" clean
+IF %ERRORLEVEL% NEQ 0 (echo Error:%ERRORLEVEL% && exit)
+
+ECHO "All assemblies cleaned successfully."
